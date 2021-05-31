7494578e3d9651c9735d4720a47351048be5a04b
mjaiswal <mjaiswal@womply.com>
2021-06-01 00:53:26 +0530

    Virtual Mouse - move, left and right click features

---
+++ AIVirtualMouse/CameraTest.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import HandTrackingModule as htm
+import time
+import autopy
+
+##########################
+wCam, hCam = 640, 480
+frameR = 100  # Frame Reduction
+smoothening = 7
+#########################
+
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
